a43a8bd2eb222689b8e5c251c2bafbb3ed4d3694
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-08-20 13:55:19 +1000

    Delete app/src/main/java/com/example/rostermanagementandroidsystem/entity directory

--- app/src/main/java/com/example/rostermanagementandroidsystem/entity/RoleEntity.kt
+++
@@ -1,9 +0,0 @@
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "roles")
-data class RoleEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "name") val name: String
-)
